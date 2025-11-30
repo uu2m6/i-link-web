a5aab6726c05c5736532657868860fc4165b5d1d
Kim eojin <151831170+uu2m6@users.noreply.github.com>
2025-12-01 01:15:40 +0900

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': { 
-        target: 'https://a9c638c029b8.ngrok-free.app',
+        target: 'https://98905c106a71.ngrok-free.app',
         changeOrigin: true,
       }
     }

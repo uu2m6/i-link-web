f53dd365f7fd7cbaa08924df453e219d6ac8831a
Kim eojin <151831170+uu2m6@users.noreply.github.com>
2025-12-30 20:11:43 +0900

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': { 
-        target: 'https://7c8e3485e676.ngrok-free.app', //작은 따옴표 안에 API링크 삽입
+        target: 'https://12f639c6c828.ngrok-free.app', //작은 따옴표 안에 API링크 삽입
         changeOrigin: true,
       }
     }
